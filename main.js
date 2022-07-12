390bd09bad2946904b38a12acca244f676700fc1
Kenny <devkenny@outlook.com>
2022-07-12 13:28:59 +1000

    revert changes on return (not complete)

--- main.js
+++ main.js
@@ -54,6 +54,9 @@ function Reset_Stage(stage) {
       ).style.cursor = `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg'  width='32' height='38' viewport='0 0 100 100' style='fill:black;font-size:19px;'><text y='50%'>🟡</text></svg>") 16 0,auto`;
     default:
       break;
+    case 4:
+      Disable_Continue_Button(4);
+      document.getElementById("stage_4-key").textContent = "🗝️";
   }
 }
 
