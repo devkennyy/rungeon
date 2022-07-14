259de9ca04bdaa5d3ad266f3045472d9dcf2a992
Kenny <devkenny@outlook.com>
2022-07-14 15:00:02 +1000

    use theme on rungeon.html

--- main.js
+++ main.js
@@ -122,13 +122,39 @@ function OpenThemePopup() {
 
 function SetTheme(event) {
   document.getElementById(
-    "themeStylesheet"
+    "indexThemeStylesheet"
   ).href = `styles/themes/${event.target.id}.css`;
+  localStorage.setItem("index_theme", `${event.target.id}`);
+  localStorage.setItem("theme", `${event.target.id}`);
   CloseThemePopup();
 }
 
 function CloseThemePopup() {
   document.getElementById("themePopup").hidden = true;
+}
+// index.html theme check
+if (localStorage.getItem("index_theme") == null) {
+  console.log("theme was null");
+  document.getElementById("themeStylesheet").href = `styles/themes/default.css`;
+} else {
+  console.log("Getting index themes...");
+  let index_theme = localStorage.getItem("index_theme");
+  document.getElementById(
+    "themeStylesheet"
+  ).href = `styles/themes/${index_theme}.css`;
+}
+
+// rungeon.html theme check
+console.log(document.getElementById("themeStylesheet"));
+if (localStorage.getItem("theme") == null) {
+  console.log("theme was null");
+  document.getElementById("themeStylesheet").href = `styles/themes/default.css`;
+} else {
+  console.log("Getting rungeon themes...");
+  let theme = localStorage.getItem("theme");
+  document.getElementById(
+    "themeStylesheet"
+  ).href = `styles/themes/${theme}.css`;
 }
 
 function Update_Title_Progress() {
