d211ac35a13c0cddd5b114683eaae55bfbec5092
Steve <steve1818fernandes@gmail.com>
2022-07-16 15:03:57 +0530

    A little hard to tell if a button is disabled for mobile #61

--- main.js
+++ main.js
@@ -96,6 +96,7 @@ function Next_Stage(currentStage) {
 
 function Enable_Continue_Button(currentStage) {
   document.getElementById(`stage_${currentStage}-btn`).disabled = false;
+  document.getElementById(`stage_${currentStage}-btn`).innerHTML = "Continue";
 }
 
 function Disable_Continue_Button(currentStage) {
@@ -231,6 +232,7 @@ function Stage_Two_Coin() {
 /* STAGE THREE */
 function Stage_Three_Coin() {
   if (stageTwo_hasCoin) {
+    Enable_Continue_Button(3);
     document.getElementById("stage_3-btn").disabled = false;
     document.getElementById("stage_3-icon").style.cursor = "auto";
   }
