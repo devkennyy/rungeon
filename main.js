ca37e3163ebbf07451ac68462f448c193cd8e3be
GruelingPine185 <66073333+GruelingPine185@users.noreply.github.com>
2022-07-17 18:05:36 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 const express = require("express");
-const { appendFile } = require("fs");
 const rungeon = express();
 const port = 3000;
 
 
-rungeon.engine('html', require('ejs').renderFile)
-
+rungeon.engine('html', require('ejs').renderFile);
 rungeon.use(express.static("public"));
-
 
 rungeon.listen(port, () => {
     console.log(`listening on port ${port}`);
