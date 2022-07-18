a433303febf350971024984aeb06e4bd89cf4cdd
GruelingPine185 <66073333+GruelingPine185@users.noreply.github.com>
2022-07-17 20:54:47 -0400

    added express backend

---
+++ main.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const rungeon = express();
+const port = 3000;
+
+
+rungeon.use(express.static(__dirname + '/public'));
+
+rungeon.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
