8a01e4d32b371a13e4af87df4fdc6640ac0ba614
GruelingPine185 <66073333+GruelingPine185@users.noreply.github.com>
2022-07-17 22:43:49 -0400

    achievements page

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const { appendFile } = require("fs");
 const rungeon = express();
 const port = 3000;
 
 
 rungeon.use(express.static(__dirname + '/public'));
+rungeon.set('view engine', 'ejs');
+
+
+rungeon.get("/achievements", (req, res) => {
+    res.render("achievements");
+});
 
 rungeon.listen(port, () => {
     console.log(`Listening on port ${port}`);
