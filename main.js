e2956584cdcec380cd7941099553cc6939da48de
GruelingPine185 <66073333+GruelingPine185@users.noreply.github.com>
2022-07-20 16:43:29 -0400

    moved and added login route

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-/* eslint-env node */
-
-const express = require('express');
+const express = require("express");
 const rungeon = express();
 const port = 3000;
 
 
-rungeon.use(express.static(__dirname + '/public'));
+rungeon.use(express.static(__dirname + "/public"));
+rungeon.set('view engine', 'ejs');
+
+rungeon.get("/login", (req, res) => {
+    res.render("login", {
+        title: "Rungeon - Login"
+    });
+});
 
 rungeon.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+    console.log(`Listening on port ${port}`);
 });
