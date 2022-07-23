31a28d4fe2220bf2b89b22d7eb9318683290ea73
Kenny <devkenny@outlook.com>
2022-07-24 07:05:46 +1000

    linter fixes

--- main.js
+++ main.js
@@ -1,43 +1,43 @@
-const express = require("express");
+const express = require('express');
 const rungeon = express();
 const port = 3000;
 
 
-rungeon.use(express.static(__dirname + "/public"));
+rungeon.use(express.static(__dirname + '/public'));
 rungeon.set('view engine', 'ejs');
 
 
-rungeon.get("/", (req, res) => {
-    res.render("index", {
-        title: "Rungeon"
-    });
+rungeon.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Rungeon'
+  });
 });
 
-rungeon.get("/login", (req, res) => {
-    res.render("login", {
-        title: "Rungeon - Login"
-    });
+rungeon.get('/login', (req, res) => {
+  res.render('login', {
+    title: 'Rungeon - Login'
+  });
 });
 
-rungeon.get("/signup", (req, res) => {
-    res.render("signup", {
-        title: "Rungeon - SignUp"
-    });
+rungeon.get('/signup', (req, res) => {
+  res.render('signup', {
+    title: 'Rungeon - SignUp'
+  });
 });
 
-rungeon.get("/rungeon", (req, res) => {
-    res.render("rungeon", {
-        title: "Rungeon - Game"
-    });
+rungeon.get('/rungeon', (req, res) => {
+  res.render('rungeon', {
+    title: 'Rungeon - Game'
+  });
 });
 
-rungeon.get("*", (req, res) => {
-    res.status(404);
-    res.render("404", {
-        title: "Rungeon - Page Not Found"
-    });
+rungeon.get('*', (req, res) => {
+  res.status(404);
+  res.render('404', {
+    title: 'Rungeon - Page Not Found'
+  });
 });
 
 rungeon.listen(port, () => {
-    console.log(`Listening on port ${port} \nctrl + click: http://localhost:3000/`);
+  console.log(`Listening on port ${port} \nctrl + click: http://localhost:3000/`);
 });
