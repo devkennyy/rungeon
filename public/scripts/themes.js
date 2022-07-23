958a6b8f96ddb7a4e3bade8a93cef269fd356132
GruelingPine185 <66073333+GruelingPine185@users.noreply.github.com>
2022-07-23 17:20:56 -0400

    fixed theme dropdown

--- public/scripts/themes.js
+++ public/scripts/themes.js
@@ -53,6 +53,12 @@ const setTheme = (theme) => {
 
 const getTheme = (oldTheme) => {
   const newTheme = localStorage.getItem('theme');
+  if (newTheme !== undefined) {
+    document.body.classList.add(`theme-${newTheme}`);
+    console.log(`theme: ${newTheme}`);
+    return;
+  }
+
   document.body.classList.replace(`theme-${oldTheme}`, `theme-${newTheme}`);
   console.log(`theme: ${newTheme}`);
 };
