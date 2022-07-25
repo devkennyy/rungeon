ef5465f247fd6de7be56ca5b11fc8f0137138884
Terrance Brown <terrancem.brown@gmail.com>
2022-07-24 21:29:32 -0500

    tightened configs for prettier and eslint

--- public/scripts/themes.js
+++ public/scripts/themes.js
@@ -7,7 +7,7 @@ const themes = [
   "rgb",
   "serika",
   "default",
-]
+];
 
 /**
  * Determines if provided theme names are the unique and therefore would
@@ -18,53 +18,53 @@ const themes = [
  */
 const checkNewTheme = (currentTheme, theme) => {
   if (currentTheme === theme) {
-    console.log("same theme")
-    return 0
+    console.log("same theme");
+    return 0;
   } else if (currentTheme === null) {
-    localStorage.setItem("theme", "default")
-    console.log("set default theme")
-    return 0
+    localStorage.setItem("theme", "default");
+    console.log("set default theme");
+    return 0;
   }
-  return 1
-}
+  return 1;
+};
 
 /**
  * Sets the website theme as long as it exists. If the requested theme
  * is the same as the current theme, no operation is done
  */
 const setTheme = (theme) => {
-  const currTheme = localStorage.getItem("theme")
-  if (!checkNewTheme(currTheme, theme)) return
+  const currTheme = localStorage.getItem("theme");
+  if (!checkNewTheme(currTheme, theme)) return;
 
   switch (theme) {
     case themes[0]:
-      localStorage.setItem("theme", themes[0])
-      break
+      localStorage.setItem("theme", themes[0]);
+      break;
     case themes[1]:
-      localStorage.setItem("theme", themes[1])
-      break
+      localStorage.setItem("theme", themes[1]);
+      break;
     case themes[2]:
-      localStorage.setItem("theme", themes[2])
-      break
+      localStorage.setItem("theme", themes[2]);
+      break;
     case themes[3]:
-      localStorage.setItem("theme", themes[3])
-      break
+      localStorage.setItem("theme", themes[3]);
+      break;
     case themes[4]:
-      localStorage.setItem("theme", themes[4])
-      break
+      localStorage.setItem("theme", themes[4]);
+      break;
     case themes[5]:
-      localStorage.setItem("theme", themes[5])
-      break
+      localStorage.setItem("theme", themes[5]);
+      break;
     default:
-      localStorage.setItem("theme", themes[6])
-      break
+      localStorage.setItem("theme", themes[6]);
+      break;
   }
 
-  getTheme(currTheme)
-}
+  getTheme(currTheme);
+};
 
 const getTheme = (oldTheme) => {
-  const newTheme = localStorage.getItem("theme")
-  document.body.classList.replace(`theme-${oldTheme}`, `theme-${newTheme}`)
-  console.log(`theme: ${newTheme}`)
-}
+  const newTheme = localStorage.getItem("theme");
+  document.body.classList.replace(`theme-${oldTheme}`, `theme-${newTheme}`);
+  console.log(`theme: ${newTheme}`);
+};
