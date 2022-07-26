a74fc0de4a892c9d0d0cc5482303039cc9833601
GruelingPine185 <66073333+GruelingPine185@users.noreply.github.com>
2022-07-26 07:17:40 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 /* eslint-env node */
+
 const express = require("express");
-const mongoose = require("mongoose");
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const User = require("./backend/models/user");
 const rungeon = express();
 const port = 3000;
 
 
 rungeon.use(express.static(__dirname + "/public"));
 rungeon.set("view engine", "ejs");
+
 
 rungeon.get("/", (req, res) => {
   res.render("index", {
@@ -17,36 +16,23 @@ rungeon.get("/", (req, res) => {
 });
 
 rungeon.get("/login", (req, res) => {
-  // const user = new User({
-  //   username: "user",
-  //   password: "test123",
-  //   email: undefined
-  // });
-  
-  // user.save()
-  //   .then((res) => {
-  //     res.send(res);
-  //   }).catch((err) => {
-  //     res.send(err);
-  //   });
-    
   res.render("login", {
     title: "Rungeon - Login"
   });
 });
-  
+
 rungeon.get("/signup", (req, res) => {
   res.render("signup", {
     title: "Rungeon - Sign Up"
   });
 });
-  
+
 rungeon.get("/rungeon", (req, res) => {
   res.render("rungeon", {
     title: "Rungeon - Game"
   });
 });
-    
+
 rungeon.get("*", (req, res) => {
   res.status(404);
   res.render("404", {
