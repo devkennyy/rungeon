e48e083e3da392431ce51b16b0818ade71af37e0
GruelingPine185 <66073333+GruelingPine185@users.noreply.github.com>
2022-07-30 07:52:08 -0400

    added icons
    lint fixes

--- public/scripts/stages.js
+++ public/scripts/stages.js
@@ -1,11 +1,14 @@
+// icons[0] is always going to be the icon above the prompt
 const stages = [
   {
     id: 0,
     title: "A mystery door, how exciting",
     body: "Maybe that button does something?",
-    icon: {
-      name: "fa-door-open"
-    },
+    icons: [
+      {
+        name: "fa-door-open"
+      }
+    ],
     back: -1,
     next: 1,
   },
@@ -13,9 +16,11 @@ const stages = [
     id: 1,
     title: "The bugs are coming",
     body: "SQUASH THE BUG! DO IT! DO IT!",
-    icon: {
-      name: "fa-bugs"
-    },
+    icons: [
+      {
+        name: "fa-bugs"
+      }
+    ],
     back: 0,
     next: 2,
   },
@@ -23,9 +28,14 @@ const stages = [
     id: 2,
     title: "Is that a dragon",
     body: "Take a breath, you've deserved it",
-    icon: {
-      name: "fa-wind"
-    },
+    icons: [
+      {
+        name: "fa-circle-question" // originally fa-wind
+      }
+      // {
+      //   name: ""
+      // }
+    ],
     back: 1,
     next: 3,
   },
@@ -33,9 +43,14 @@ const stages = [
     id: 3,
     title: "That's definitely a dragon",
     body: "Give him the coin or something!",
-    icon: {
-      name: "fa-dragon"
-    },
+    icons: [
+      {
+        name: "fa-dragon"
+      },
+      {
+        name: "🟡"
+      }
+    ],
     back: 2,
     next: 4,
   },
@@ -43,9 +58,14 @@ const stages = [
     id: 4,
     title: "The door is locked",
     body: "What's this in your pocket? I doubt it's useful",
-    icon: {
-      name: "fa-lock"
-    },
+    icons: [
+      {
+        name: "fa-lock"
+      },
+      {
+        name: "🗝️"
+      }
+    ],
     back: 3,
     next: 5,
   },
@@ -53,9 +73,11 @@ const stages = [
     id: 5,
     title: "Time to eat",
     body: "You've got to eat something, or you'll starve",
-    icon: {
-      name: "fa-bacon"
-    },
+    icons: [
+      {
+        name: "fa-bacon"
+      }
+    ],
     back: 4,
     next: 6,
   },
@@ -63,17 +85,30 @@ const stages = [
     id: 6,
     title: "You did it?",
     body: "Get out while you can.",
-    icon: {
-      name: "fa-person-running"
-    },
+    icons: [
+      {
+        name: "fa-person-running"
+      }
+    ],
     back: -1,
     next: -1,
   },
 ];
 
 const setStageText = stageData => {
-  document.getElementById("stage-title").innerText = stageData.title;
-  document.getElementById("stage-body").innerText = stageData.body;
+  switch(stageData.icons.length) {
+    case 1:
+      document.getElementById("stage-title").innerText = stageData.title;
+      document.getElementById("stage-body").innerText = stageData.body;
+      break;
+    case 2:
+      document.getElementById("stage-body")
+        .innerText = stageData.body + " " + stageData.icons[1].name;
+      break;
+    default:
+      console.log("too many icons.");
+      break;
+  }
 };
 
 const setStageButtons = stageData => {
@@ -95,7 +130,7 @@ const setStageButtons = stageData => {
 const setStageIcon = stageData => {
   let icon = document.getElementById("stage-icon");
   icon.className = "fas";
-  icon.classList.add(stageData.icon.name);
+  icon.classList.add(stageData.icons[0].name);
 };
 
 const setStage = stageData => {
